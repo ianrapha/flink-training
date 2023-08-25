232dfd7a1eee0a322fbd55ef3d7d39084e643a97
Ian Raphael <ianrapha@gmail.com>
2023-08-25 17:34:03 -0300

    Enabled Scala project support Added solution for the Ride Cleansing exercise

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
@@ -26,6 +26,7 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
+import org.apache.flink.training.exercises.common.utils.GeoUtils;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 
 /**
@@ -80,7 +81,10 @@ public class RideCleansingExercise {
     public static class NYCFilter implements FilterFunction<TaxiRide> {
         @Override
         public boolean filter(TaxiRide taxiRide) throws Exception {
-            throw new MissingSolutionException();
+            boolean rideStartsInNYC = GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat);
+            boolean rideEndsInNYC = GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
+
+            return (rideStartsInNYC && rideEndsInNYC);
         }
     }
 }
